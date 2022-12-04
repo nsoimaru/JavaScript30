eb958433600f1dbf89d3304457010fc645c99408
nsoimaru <nsoimaru@gmail.com>
2022-12-04 18:45:53 +0200

    Finish day one exercises

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,22 @@
+// TODO change keyCode with key attribute
+
+playSound = function (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (!audio) return; // stop function for running if no audio is associated with key
+    audio.currentTime = 0 // rewind if play fast multiple time
+
+    audio.play();
+    key.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+window.addEventListener('keydown', playSound);
